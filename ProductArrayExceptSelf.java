fe06477d9b752928aad45d521e82edfc653aa08a
Shreya <shreyakumari.7102005@gmail.com>
2026-02-05 20:11:08 +0530

    Appended

--- ProductArrayExceptSelf.java
+++ ProductArrayExceptSelf.java
@@ -1,29 +1,31 @@
-class Solution {
+public class ProductArrayExceptSelf {
+
     public int[] productExceptSelf(int[] nums) {
-       int answer[]=new int[nums.length];
-       for(int i=0;i<nums.length;i++){
+        int answer[] = new int[nums.length];
+
+        for(int i=0;i<nums.length;i++){
             int mul=1;
-            int a=nums[i];
+
             for(int j=0;j<nums.length;j++){
                 mul*=nums[j];
             }
-            if(nums[i]==0){
+
+            if(nums[i]==0)
                 answer[i]=mul;
-            }
-            else{
+            else
                 answer[i]=mul/nums[i];
-            }
-            
-       }
-       return answer;
+        }
+
+        return answer;
     }
-    public static void main(String[]args){
-        Solution s = new Solution();
+
+    public static void main(String[] args){
+        ProductArrayExceptSelf s = new ProductArrayExceptSelf();
+
         int nums[] = {1,2,3,4};
         int res[] = s.productExceptSelf(nums);
 
-        for(int x : res){
-            System.out.print(x + " ");
-        }
+        for(int x: res)
+            System.out.print(x+" ");
     }
 }
