998012082e448a7252b15b2bf6885df05fe083a9
Shreya <shreyakumari.7102005@gmail.com>
2026-02-22 21:38:18 +0530

    a1

--- minPairSum.java
+++ minPairSum.java
@@ -3,6 +3,9 @@ import java.util.Arrays;
 public class minPairSum {
     public int minPairSum(int[] nums) {
         Arrays.sort(nums);
+        int maxCount=0;
+        int left=0;
+        int right=nums.length-1;
         
     }
 }
