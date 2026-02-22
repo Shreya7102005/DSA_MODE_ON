8a8b50bc39ca15de7844d4130ebf847f6b1366fe
Shreya <shreyakumari.7102005@gmail.com>
2026-02-22 21:40:12 +0530

    a1

--- minPairSum.java
+++ minPairSum.java
@@ -6,6 +6,12 @@ public class minPairSum {
         int maxCount=0;
         int left=0;
         int right=nums.length-1;
-        
+        while(left>right){
+            int max=nums[left]+nums[right];
+            maxCount=Math.max(max,maxCount);
+            left++;
+            right--;
+        }
+        return maxCount;
     }
 }
