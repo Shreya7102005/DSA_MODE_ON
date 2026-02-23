9180438fef2ccbb9c35927696fb34422e66228ee
Shreya <shreyakumari.7102005@gmail.com>
2026-02-23 11:46:12 +0530

    a1

---
+++ StriversSheet/largestElement.java
@@ -0,0 +1,13 @@
+package StriversSheet;
+
+public class largestElement {
+     public int largestElement(int[] nums){
+        int max=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max){
+                max=nums[i];
+            }
+        }
+        return max;
+     }
+}
