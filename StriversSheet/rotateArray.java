39892be5d0c13e9103f101b55237e18743c38c7f
Shreya <shreyakumari.7102005@gmail.com>
2026-02-24 10:10:35 +0530

    a1

---
+++ StriversSheet/rotateArray.java
@@ -0,0 +1,20 @@
+package StriversSheet;
+
+import java.util.ArrayList;
+
+public class rotateArray {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n;
+        int a=n-k;
+        ArrayList<Integer>arr=new ArrayList<>();
+        for(int i=a;i<n;i++){
+            arr.add(nums[i]);
+        }
+        for(int i=0;i<a;i++){
+            arr.add(nums[i]);
+        }
+        
+    }
+    
+}
