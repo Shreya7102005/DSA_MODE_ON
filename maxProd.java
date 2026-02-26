c6aa857e2a9857bbf9afc25cbe7945ae1aa8854e
Shreya <shreyakumari.7102005@gmail.com>
2026-02-26 21:52:39 +0530

    a1

---
+++ maxProd.java
@@ -0,0 +1,16 @@
+public class maxProd {
+    public int maxProduct(int[] nums) {
+        int maxProduct = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int prod = 1;
+
+            for (int j = i; j < nums.length; j++) {
+                prod *= nums[j];
+                maxProduct = Math.max(maxProduct, prod);
+            }
+        }
+
+        return maxProduct;
+    }
+}
