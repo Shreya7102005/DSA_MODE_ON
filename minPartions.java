95a22a8a99f751d6269f3b701c7c3dda500e7bee
Shreya <shreyakumari.7102005@gmail.com>
2026-03-01 22:14:44 +0530

    a1

--- minPartions.java
+++ minPartions.java
@@ -1,5 +1,10 @@
 class minPartions{
     public int minPartitions(String n) {
-        
+        int maxDigit=0;
+        for(int i=0;i<n.length();i++){
+            int dig=n.charAt(i)-'0';
+            maxDigit=Math.max(maxDigit,dig);
+        }
+        return maxDigit;
     }
 }
