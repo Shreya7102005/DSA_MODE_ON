f07d669057ea2fe1b8282b0e02da1642e00be2a9
Shreya <shreyakumari.7102005@gmail.com>
2026-03-04 22:16:03 +0530

    mirror Distacne

---
+++ StriversSheet/mirrorDistance.java
@@ -0,0 +1,12 @@
+public class mirrorDistance {
+    public int mirrorDistance(int n) {
+        int rev=0;
+        int a=n;
+        while(n>0){
+            int r=n%10;
+            rev=rev*10+r;
+            n/=10;
+        }
+        return Math.abs(a-rev);
+    }
+}
