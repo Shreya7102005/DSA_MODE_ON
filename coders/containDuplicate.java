d5db60582310ba1259ea0e6fcb4208caa7485582
Shreya <shreyakumari.7102005@gmail.com>
2026-03-08 15:50:42 +0530

    Contain duplicate

--- coders/containDuplicate.java
+++ coders/containDuplicate.java
@@ -1,12 +1,23 @@
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class containDuplicate {
-    public boolean containsDuplicate(int[] nums) {
+    /*public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
         for(int i=1;i<nums.length;i++){
             if(nums[i-1]==nums[i]){
                 return true;
             }
+        }
+        return false;
+    }*/
+   public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer>set=new HashSet<>();
+        for(int n:nums){
+            if(set.contains(n)){
+                return true;
+            }
+            set.add(n);
         }
         return false;
     }
