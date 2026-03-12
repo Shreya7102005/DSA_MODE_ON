87fa56241c8ad9d2c542299260edb8e8646cf64d
Shreya <shreyakumari.7102005@gmail.com>
2026-03-12 08:36:55 +0530

    kth positive

---
+++ kthPositive.java
@@ -0,0 +1,11 @@
+class kthPositive{
+    public int findKthPositive(int[] arr, int k) {
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]-(i+1)>=k){
+                return i+k;
+            }
+
+        }
+        return arr.length+k;
+    }
+}
