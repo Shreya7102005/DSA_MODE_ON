43eb3d2bce013a4455bebe4a3c881afbecec0786
Shreya <shreyakumari.7102005@gmail.com>
2026-03-12 21:55:02 +0530

    maximum average of a k length subarray

---
+++ findMaxAvg.java
@@ -0,0 +1,17 @@
+class findMaxAvg{
+    public double findMaxAverage(int[] nums, int k) {
+        int sum=0;
+        int maxSum;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        maxSum=sum;
+        for(int i=k;i<nums.length;i++){
+            sum+=nums[i];
+            sum-=nums[i-k];
+            maxSum=Math.max(sum,maxSum);
+        }
+        return (double)maxSum/k;
+
+    }
+}
